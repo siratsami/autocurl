c8d61219ee72a13478f676bef9b92b624e9a10cd
siratsami <60298069+siratsami@users.noreply.github.com>
2021-05-10 16:29:28 +0300

    Create autocurl.sh

---
+++ autocurl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Automated bash script to grab http resposne banners"
+echo "This script grabs the http status code as https, you can edit it"
+echo "Usage: bash autocurl.sh host-list.txt"
+
+#The cat command prints the url list
+#then we use -sI curl flag so we can see only the http response
+#and we made an http:// to make an url
+#grep works to only print the HTTP/
+
+for host in $(cat $1); do curl -sI https://$host/ | grep -e 'HTTP/' && echo "HOST: $sub" | grep -A1 HOST; done
